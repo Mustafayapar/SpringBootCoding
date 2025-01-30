1415f2077365dd94f91e85f6109b9f7d54f829a7
mustafay <mustafa_yapar99@hotmail.com>
2025-01-30 13:52:42 +0300

    Initial commit

---
+++ src/main/java/com/mustafayapar/SpringBootCodingApplication.java
@@ -0,0 +1,13 @@
+package com.mustafayapar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafayapar/SpringBootCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafayapar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
