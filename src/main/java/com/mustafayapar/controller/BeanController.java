c10b530b40a0dae83219903ec819e377ccc307f0
mustafay <mustafa_yapar99@hotmail.com>
2025-02-10 17:56:52 +0300

    Post Construct

--- src/main/java/com/mustafayapar/controller/BeanController.java
+++ src/main/java/com/mustafayapar/controller/BeanController.java
@@ -1,16 +1,21 @@
 package com.mustafayapar.controller;
 
 import com.mustafayapar.bean.BeanConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 @Controller
 public class BeanController {
 
     @Autowired
     BeanConfig beanConfig;
+
+
 
     @GetMapping("/bean/beanDto") //http://localhost:8080/bean/beanDto
     @ResponseBody // html olmadan gösterir
---
+++ src/main/java/com/mustafayapar/controller/PostConstructorTuto.java
@@ -0,0 +1,27 @@
+package com.mustafayapar.controller;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.logging.Logger;
+
+
+@Controller
+public class PostConstructorTuto {
+
+    @Autowired
+    Logger LOG;
+
+    //    public PostConstructorTuto() {
+//        LOG.info("Log info");
+//    }
+    @PostConstruct
+    public void init(){
+        LOG.info("Log info");
+    }
+    public static void main(String[] args) {
+        BeanController beanController = new BeanController();
+        System.out.println(beanController);
+    }
+}
