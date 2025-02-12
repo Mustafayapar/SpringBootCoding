4be69cfdd02e62ae99f97537c67af2679f740cbc
mustafay <mustafa_yapar99@hotmail.com>
2025-02-13 00:05:01 +0300

    tymeleaf css js

--- src/main/java/com/mustafayapar/controller/TymeleafController.java
+++ src/main/java/com/mustafayapar/controller/TymeleafController.java
@@ -1,6 +1,7 @@
 package com.mustafayapar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,6 +12,24 @@ public class TymeleafController {
     @GetMapping("/tymeleaf1")
     public String getTymeleaf() {
 
-        return "tymeleaf1 dfds";
+        return "tymeleaf1";
     }
+
+    //http://localhost:8080/tymeleaf2
+    @GetMapping("/tymeleaf2")
+    public String getThymeleaf2Model(Model model) {
+
+        model.addAttribute("key_model" , "ben modelden geldim");
+        return "tymeleaf1";
+    }
+
+    //http://localhost:8080/tymeleaf3
+    @GetMapping("/tymeleaf3")
+    public String getThymeleaf3Model(Model model) {
+
+        model.addAttribute("key_model" , "ben modelden geldim");
+        return "tyhmwleaf_file/tymeleaf1";
+    }
+
+
 }
